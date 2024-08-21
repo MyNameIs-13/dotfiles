e9d8eafbbe2dcbaa9108c2a254e8e9bfef2f1240
MyNameIs-13 <g.s#git@writeme.com>
2024-08-21 18:19:10 +0200

    config changed

--- chezmoi/.chezmoiscripts/run_once_after_get-kdbx.sh
+++ chezmoi/.chezmoiscripts/run_once_after_get-kdbx.sh
@@ -2,9 +2,7 @@
 kdbx="/home/${USER}/documents/confidental/GSKPasswordList.kdbx"
 
 if [[ ! -f "${kdbx}" ]]; then
-    echo "Error: File ${kdbx} does not exist."
-    exit 1
+    mkdir -p "/home/${USER}/documents/confidental"
+    scp pi1:/srv/dev-disk-by-uuid-fed98fee-2f27-422f-ac75-e73ee6e0d12e/appdata/syncthing/data/Documents/scm/system_install/secrets/GSKPasswordList.kdbx "${kdbx}"
 fi
 
-mkdir -p "/home/${USER}/documents/confidental"
-scp pi1:/srv/dev-disk-by-uuid-fed98fee-2f27-422f-ac75-e73ee6e0d12e/appdata/syncthing/data/Documents/scm/system_install/secrets/GSKPasswordList.kdbx "${kdbx}"
